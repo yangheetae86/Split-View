2abfe3c9bdf47ee625c5893be91f63014c1f8b5b
heroparkjisung2 <heroparkjisung2@gmail.com>
2020-05-05 22:01:53 +0900

    Initial Commit

---
+++ Split View/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Split View
+//
+//  Created by JU HAN LEE on 2020/05/05.
+//  Copyright © 2020 yht. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
